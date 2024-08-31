9f003e4bf5d99ef2beb2140f2f1c6bd4eba6fa7c
webdevyoo <yjk7454@naver.com>
2024-09-01 00:41:02 +0900

    feat: `{root module}/application.java` - 형제 모듈 컴포넌트 옵션 적용스캔

--- module-application/src/main/java/com/fc/moduleapplication/ModuleApplication.java
+++ module-application/src/main/java/com/fc/moduleapplication/ModuleApplication.java
@@ -3,7 +3,10 @@ package com.fc.moduleapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        /* 모든 모듈을 다 스캔하는 것은 시간도 오래걸리고 굉장히 비효율적이기 때문에, 빈으로 등록해야 되는 필요한 것들만 명시한다. */
+        scanBasePackages = {"com.fc.moduleredis"}
+)
 public class ModuleApplication {
 
     public static void main(String[] args) {
