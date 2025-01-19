9f00b734706f5fd5013cc824084057318e98e8e1
webdevyoo <yjk7454@naver.com>
2025-01-19 17:59:33 +0900

    feat: Ch05. 프로덕트에 대한 vo 클래스 추가

---
+++ module-application/src/main/java/com/fc/moduleapplication/vo/Product.java
@@ -0,0 +1,9 @@
+package com.fc.moduleapplication.vo;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String productId; // GUID - 21fa823e-edaa-4d81-8203-ab70b29812cc
+    private int price; // 25000 (WON)
+}
