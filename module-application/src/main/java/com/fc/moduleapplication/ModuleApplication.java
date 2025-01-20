afb4dd5c1982b2bb6e9bc8cf296781a993d01870
webdevyoo <yjk7454@naver.com>
2025-01-21 00:21:08 +0900

    feat: springboot 스레드 실행 클래스 현재 패키지 기준 경로 모듈 스캔 추가

--- module-application/src/main/java/com/fc/moduleapplication/ModuleApplication.java
+++ module-application/src/main/java/com/fc/moduleapplication/ModuleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
         /* 모든 모듈을 다 스캔하는 것은 시간도 오래걸리고 굉장히 비효율적이기 때문에, 빈으로 등록해야 되는 필요한 것들만 명시한다. */
-        scanBasePackages = {"com.fc.moduleredis", "com.fc.modulekafka"}
+        scanBasePackages = {"com.fc.moduleapplication", "com.fc.moduleredis", "com.fc.modulekafka"}
 )
 public class ModuleApplication {
 
